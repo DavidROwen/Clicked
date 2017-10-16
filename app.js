9f41185bfc190a02faccd15f664c9a2e301500e4
davidrowen <d4v1d.0w3n@gmail.com>
2017-10-16 20:49:11 +0000

    Add some js functionality

--- app.js
+++ app.js
@@ -1,2 +1,37 @@
 angular.module('clicker', [])
-.controller("clickCtrl", []);
+.controller("clickCtrl", ['$scope',
+    function($scope){
+        $scope.rps = 0;
+        $scope.rockets = 1;
+        $scope.rocketCost = 20;
+        $scope.trucks = 1;
+        $scope.truckCost = 5;
+        $scope.pads = 1;
+        $scope.padCost = 10;
+        $scope.motivation = 0;
+        $scope.money = 20;
+
+
+        $scope.addRocket = function(){
+            if ($scope.rocketCost <= $scope.money){
+                $scope.rockets++;
+            }
+        }
+        $scope.addFuelTruck = function(){
+            if ($scope.truckCost <= $scope.money){
+                $scope.trucks++;
+            }
+        }
+        $scope.addLaunchPad = function(){
+            if ($scope.padCost <= $scope.money){
+                $scope.pads++;
+            }
+        }
+        $scope.workHarder = function(){
+            $scope.motivation++;
+        }
+        $scope.launchRocket = function(){
+            $scope.money++;
+        }
+    }
+]);
