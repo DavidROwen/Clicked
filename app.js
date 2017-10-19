c00ac84a86339d60890b3b1f3585cff50e2a902d
jared-haviland <jared_haviland@byu.edu>
2017-10-19 19:47:33 +0000

    Updated files

--- app.js
+++ app.js
@@ -2,27 +2,30 @@ angular.module('clicker', [])
 .controller("clickCtrl", ['$scope', '$interval', 
     function($scope, $interval){
         
-        $scope.rps = 0;
+        $scope.rps = 0.1;
         $scope.rockets = 1;
         $scope.rocketCost = 20;
-        $scope.rocketMult = 5
+        $scope.rocketMult = 5;
+        $scope.moneyPerRocket = 5;
         $scope.trucks = 1;
         $scope.truckCost = 5;
         $scope.truckMult = 5
-        $scope.pads = 1;
-        $scope.padCost = 10;
-        $scope.padMult = 5
+        $scope.scientists = 1;
+        $scope.scientistCost = 10;
+        $scope.scientistMult = 5;
         $scope.motivation = 0;
         $scope.money = 20;
         $scope.timePerRocket = 30000; 
         $scope.timeMulti = 0;
+        $scope.totalTimeToLaunch = 10;
+        $scope.timeToLaunch = 10;
         
         // Adds a rocket and modifies the rocket rate
         $scope.addRocket = function(){
             if ($scope.rocketCost <= $scope.money){
                 $scope.rockets++;
                 $scope.money = $scope.money - $scope.rocketCost;
-                $scope.timeMulti = $scope.timeMulti + $scope.rocketMult;
+                //$scope.timeMulti = $scope.timeMulti + $scope.rocketMult;
                 $scope.updateTime()
 
             }
@@ -33,38 +36,49 @@ angular.module('clicker', [])
             if ($scope.truckCost <= $scope.money){
                 $scope.trucks++;
                 $scope.money = $scope.money - $scope.truckCost;
-                $scope.timeMulti = $scope.timeMulti + $scope.truckMult;
+                $scope.totalTimeToLaunch = ($scope.totalTimeToLaunch - 0.2).toFixed(2);
+                //$scope.timeMulti = $scope.timeMulti + $scope.truckMult;
                 $scope.updateTime()
             }
         }
 
         // Adds a launch pad and modifies the rocket rate
-        $scope.addLaunchPad = function(){
+        $scope.addScientist = function(){
             if ($scope.padCost <= $scope.money){
                 $scope.pads++;
-                $scope.money = $scope.money - $scope.padCost;
-                $scope.timeMulti = $scope.timeMulti + $scope.padMult;
+                $scope.money = $scope.money - $scope.scientistCost;
+                $scope.moneyPerRocket = ($scope.moneyPerRocket + 0.5).toFixed(2);
+                //$scope.timeMulti = $scope.timeMulti + $scope.padMult;
                 $scope.updateTime()
             }
         }
 
         // Implement some effect for motivation to do
-        $scope.workHarder = function(){
-            $scope.motivation++;  
+        $scope.panhandle = function() {
+            $scope.money++;  
         }
 
         // Call this every second or at least as frequently as each rocket is ready to launch
-        $scope.launchRocket = function(){
+        $scope.launchRockets = function(){
             // Animate a launching rocket here
-            console.log("launch rocket interval occurredf");
-            $scope.money++;
+            console.log("launch rocket interval occurred");
+            $scope.money += ($scope.rockets * $scope.moneyPerRocket);
         }
-        
-        $interval( function(){ $scope.launchRocket();}, 1000);
 
         // Changes the percentage that 
-        $scope.updateTime = function(){
-            $scope.timePerRocket = $scope.timePerRocket*($scope.timeMulti/100); 
+        $scope.updateTime = function() {
+            $scope.rps = 3;
         }
+
+        $interval( function(){ 
+            $scope.timeToLaunch = ($scope.timeToLaunch - 0.01).toFixed(2);
+        }, 10);
+
+        $scope.$watch('timeToLaunch', function() {
+            if ($scope.timeToLaunch == 0) {
+                $scope.timeToLaunch = $scope.totalTimeToLaunch;
+                $scope.launchRockets();
+            }
+        });
     }
 ]);
